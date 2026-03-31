3b74855ca0ba858a885c90cd9a295c8e21287533
changche <hsnu115023@gmail.com>
2026-03-31 15:52:47 +0800

    Set public base URL for GitHub Pages

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   mode: "cloud",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbxuQXrFJ_pEBZ2Bh7miRjRHw1Bg6mNa-3RYnmoq-wbTdpLZaTpWjmCyGe4W5wsKfgQqKw/exec",
-  publicBaseUrl: "",
+  publicBaseUrl: "https://ntpc-uas.github.io/coachflow/",
   requestTimeoutMs: 12000
 };
